d5d38b123aa663956dcde5fe2771b5b63520259f
Tiffany Messer <tiffanymesser15@gmail.com>
2023-07-25 14:38:24 -0400

    Hello world is working

--- bookapi/build.sbt
+++ bookapi/build.sbt
@@ -1,7 +1,10 @@
+import smithy4s.codegen.Smithy4sCodegenPlugin
+
 val scala3Version = "3.3.0"
 
 lazy val root = project
   .in(file("."))
+  .enablePlugins(Smithy4sCodegenPlugin)
   .settings(
     name := "BookApi",
     version := "0.1.0-SNAPSHOT",
@@ -10,3 +13,10 @@ lazy val root = project
 
     libraryDependencies += "org.scalameta" %% "munit" % "0.7.29" % Test
   )
+
+    libraryDependencies ++= Seq(
+      "com.disneystreaming.smithy4s" %% "smithy4s-http4s" % smithy4sVersion.value,
+      "com.disneystreaming.smithy4s" %% "smithy4s-http4s-swagger" % smithy4sVersion.value,
+      "org.http4s" %% "http4s-ember-server" % "0.23.18"
+    )
+
--- bookapi/src/main/scala/Main.scala
+++ bookapi/src/main/scala/Main.scala
@@ -1,5 +1,36 @@
-@main def hello: Unit =
-  println("Hello world!")
-  println(msg)
+package scala;
 
-def msg = "I was compiled by Scala 3. :)"
+import smithy4s.hello._
+import cats.effect._
+import cats.implicits._
+import org.http4s.implicits._
+import org.http4s.ember.server._
+import org.http4s._
+import com.comcast.ip4s._
+import smithy4s.http4s.SimpleRestJsonBuilder
+
+object Routes {
+  private val example: Resource[IO, HttpRoutes[IO]] =
+    SimpleRestJsonBuilder.routes(HelloWorldImpl).resource
+
+  private val docs: HttpRoutes[IO] =
+    smithy4s.http4s.swagger.docs[IO](HelloWorldService)
+
+  val all: Resource[IO, HttpRoutes[IO]] = example.map(_ <+> docs)
+}
+
+object Main extends IOApp.Simple {
+  println("hello world");
+
+  val run = Routes.all
+    .flatMap { routes =>
+      EmberServerBuilder
+        .default[IO]
+        .withPort(port"9000")
+        .withHost(host"localhost")
+        .withHttpApp(routes.orNotFound)
+        .build
+    }
+    .use(_ => IO.never)
+
+}
